257e1230dcafd12cda1db5795e674e2bce6ea5ca
bugobliterator <siddharthbharatpurohit@gmail.com>
2025-02-28 09:58:10 +1100

    add feature block UI element

--- webview-ui/src/lib/FeatureBlock.svelte
+++ webview-ui/src/lib/FeatureBlock.svelte
@@ -6,23 +6,19 @@
 	let { selected = $bindable(), features, ...props } = $props();
 
 	let checkbox: {[feature: string]: any} = $state({});
+	let mainCheckbox: any = $state(null);
 
-	let mainCheckbox: any;
-	let selectedFiltered: string[] = [];
-	for (const feature of features) {
-		if (selected.includes(feature.define)) {
-			selectedFiltered = [...selectedFiltered, feature.define];
-		}
-	}
-	console.log(selectedFiltered);
 	$effect(() => {
 		Object.keys(checkbox).forEach((key) => {
 			checkbox[key].addEventListener('change', () => {
 				if (checkbox[key].checked) {
+					selected = selected.filter((item: string) => item !== `!${key}`);
 					selected = [...selected, key];
 				} else {
 					selected = selected.filter((item: string) => item !== key);
+					selected = [...selected, `!${key}`];
 				}
+				console.log(selected);
 				// check if all checkboxes are checked
 				var mainCheckbox = document.getElementById(features[0].category);
 				if (mainCheckbox) {
@@ -38,19 +34,30 @@
 		});
 	});
 
+	function updateMainCheckbox(mainCheckbox: any) {
+		let selectedFiltered: string[] = [];
+		for (const feature of features) {
+			if (selected.includes(feature.define)) {
+				selectedFiltered = [...selectedFiltered, feature.define];
+			}
+		}
+		// check if all features in the selected list
+		if (selectedFiltered.length === features.length) {
+			mainCheckbox.checked = true;
+			mainCheckbox.indeterminate = false;
+		} else if (selectedFiltered.length === 0) {
+			mainCheckbox.checked = false;
+			mainCheckbox.indeterminate = false;
+		} else {
+			mainCheckbox.indeterminate = true;
+		}
+	}
+
 	$effect(() => {
 		if (mainCheckbox) {
 			mainCheckbox.addEventListener('change', () => {
 			});
-			
-			// check if all features in the selected list
-			if (selectedFiltered.length === features.length) {
-				mainCheckbox.checked = true;
-			} else if (selectedFiltered.length === 0) {
-				mainCheckbox.checked = false;
-			} else {
-				mainCheckbox.indeterminate = true;
-			}
+			updateMainCheckbox(mainCheckbox);
 		}
 	});
 </script>
@@ -59,11 +66,11 @@
 <vscode-checkbox class="feature-checkbox" bind:this={mainCheckbox}></vscode-checkbox>
 <vscode-collapsible class="collapsible-block"  title={features[0].category}>
 	{#each features as feature}
-		<div style="padding:2px">
+		<div class="checkbox-item">
 		{#if selected.includes(feature.define)}
-			<vscode-checkbox bind:this={checkbox[feature.label]} value={feature.define} checked>{feature.description}</vscode-checkbox>
+			<vscode-checkbox bind:this={checkbox[feature.define]} value={feature.define} checked>{feature.description}</vscode-checkbox>
 		{:else}
-			<vscode-checkbox bind:this={checkbox[feature.label]} value={feature.define}>{feature.description}</vscode-checkbox>
+			<vscode-checkbox bind:this={checkbox[feature.define]} value={feature.define}>{feature.description}</vscode-checkbox>
 		{/if}
 		</div>
 	{/each}
@@ -86,5 +93,8 @@
 		height:100%;
 		width:75%;
 		vertical-align:middle;
+	}
+	.checkbox-item {
+		padding:2px;
 	}
 </style>
