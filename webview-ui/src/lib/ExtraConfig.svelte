d5501da863dc80740d11bac0593989477c1800d8
bugobliterator <siddharthbharatpurohit@gmail.com>
2025-04-06 20:49:33 +1000

    fix setting configureOptions using Extra Config fields

--- webview-ui/src/lib/ExtraConfig.svelte
+++ webview-ui/src/lib/ExtraConfig.svelte
@@ -2,16 +2,24 @@
 	import "@vscode-elements/elements/dist/vscode-textfield/index.js";
 	import "@vscode-elements/elements/dist/vscode-label/index.js";
 
-	let { value = $bindable(),...props} = $props();
+	let { value = $bindable(), ...props } = $props();
 
 	let textField: any = $state(null);
 
 	$effect(() => {
-		textField?.addEventListener('change', () => {
+		textField?.addEventListener("change", () => {
 			value = textField.value;
 		});
+	});
+
+	// Update the textfield value when the prop value changes
+	$effect(() => {
+		if (textField && value !== undefined) {
+			textField.value = value;
+		}
 	});
 </script>
 
 <vscode-label for={props.id}>{props.label}</vscode-label>
-<vscode-textfield bind:this={textField} id={props.id} type="text"></vscode-textfield>
+<vscode-textfield bind:this={textField} id={props.id} type="text"
+></vscode-textfield>
